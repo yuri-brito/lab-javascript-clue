a3b8022c2f3179dd20d00b5ffd6ffc57e86dd54e
Yuri Brito <yurisb85@gmail.com>
2022-10-20 18:07:19 -0300

    Final

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,74 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [{                       
+   'firstName':'Victor','lastName':'Plum','occupation':'','age':1,'description':'','image':'','color':''
+},{'firstName':'Victor','lastName':'Plum','occupation':'','age':1,'description':'','image':'','color':''
+},{'firstName':'Victor','lastName':'Plum','occupation':'','age':1,'description':'','image':'','color':''
+},{'firstName':'Victor','lastName':'Plum','occupation':'','age':1,'description':'','image':'','color':''
+},{'firstName':'Victor','lastName':'Plum','occupation':'','age':1,'description':'','image':'','color':''
+},{'firstName':'Victor','lastName':'Plum','occupation':'','age':1,'description':'','image':'','color':''
+
+}];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+                                   
+      {'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+    },{'name':'Billiard Room'
+      
+      }
+
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    {                       
+        'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+     },{'name':'poison','weight':1
+      
+      }
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(lista) {
+    return lista[Math.floor(Math.random()*lista.length)]
+}
 
-function pickMystery() {}
+function pickMystery() {
+    obj={'suspect':suspectsArray[Math.floor(Math.random()*suspectsArray.length)],'weapon':weaponsArray[Math.floor(Math.random()*weaponsArray.length)],'room':roomsArray[Math.floor(Math.random()*roomsArray.length)]}
+    return obj
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery() {
+    obj=pickMystery()
+    return `${obj.suspect.firstName} ${obj.suspect.lastName} killed Mr. Boddy using the ${obj.weapon.name} in the ${obj.room.name}!`
+}
 
